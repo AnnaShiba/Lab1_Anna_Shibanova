930bd216b7db82aa2163c361831e1f45a31cc3d6
Anna Shibanova <anna.shibanova@georgebrown.ca>
2025-02-09 23:49:11 -0500

    initial commit

---
+++ LabAssignment1/LabAssignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  LabAssignment1App.swift
+//  LabAssignment1
+//
+//  Created by Anna on 9.02.25.
+//
+
+import SwiftUI
+
+@main
+struct LabAssignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
