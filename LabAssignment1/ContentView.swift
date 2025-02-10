eec4c35d8967fdb744335690ad20093c27dae13c
Anna Shibanova <anna.shibanova@georgebrown.ca>
2025-02-10 00:03:23 -0500

    check answer on button press

--- LabAssignment1/ContentView.swift
+++ LabAssignment1/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number = 0
+    @State private var correct = 0
+    @State private var wrong = 0
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(wrong)")
+            
+            Button("Prime") {
+                answer(isPrime: true)
+            }
+            Button("Not Prime") {
+                answer(isPrime: false)
+            }
         }
         .padding()
+    }
+    
+    func answer(isPrime: Bool) {
+        number += 1
+        if isPrime == isPrimeNumber(n: number) {
+            correct += 1
+        } else {
+            wrong += 1
+        }
+    }
+    
+    func isPrimeNumber(n: Int) -> Bool {
+        return true
     }
 }
 
